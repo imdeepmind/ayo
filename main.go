01a0fb1f0d6808b714f09c2225fd0f5ed7ff152a
Abhishek Chatterjee <abhishekchatterjeejit@gmail.com>
2026-02-10 21:09:15 +0530

    feat: added wails

--- main.go
+++ main.go
@@ -1,103 +1,36 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"io"
-	"path/filepath"
+	"embed"
+
+	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
 )
 
-func OpenFileDescriptor(path string) *os.File {
-	file, err := os.Open(path)
-
-	if err != nil {
-		panic(fmt.Sprintf("Failed to open file descriptor: %v", err))
-	}
-
-	return file
-}
-
-func WriteChunk(index int64, chunk []byte) {
-	filename := filepath.Join("./data/chunks", fmt.Sprintf("%d", index))
-
-	out, err := os.Create(filename)
-
-	if err != nil {
-		panic(fmt.Sprintf("Failed to create file: %v", err))
-	}
-
-	defer out.Close()
-
-	_, err = out.Write(chunk)
-
-	if err != nil {
-		panic(fmt.Sprintf("Failed to write chunk: %v", err))
-	}
-}
-
-func ReconstructFromChunks() {
-	chunksDir := "./data/chunks"
-	newFile := "./data/output/image.JPG"
-
-	out, err := os.Create(newFile)
-
-	if err != nil {
-		panic(fmt.Sprintf("Failed to create file: %v", err))
-	}
-
-	defer out.Close()
-
-	var index int64 = 0
-
-	for {
-		filename := filepath.Join(chunksDir, fmt.Sprintf("%d", index))
-		
-        in, err := os.Open(filename)	
-		
-		if err != nil {
-			if os.IsNotExist(err) {
-				break
-			}
-			panic(fmt.Sprintf("Failed to open file: %v", err))
-		}
-		
-		defer in.Close()
-		
-        _, err = io.Copy(out, in)
-
-		if err != nil {
-			panic(fmt.Sprintf("Failed to copy chunk: %v", err))
-		}
-		
-		index++		
-	}
-}
+//go:embed all:frontend/dist
+var assets embed.FS
 
 func main() {
-	file := OpenFileDescriptor("./data/input/image.JPG")
-	defer file.Close()
+	// Create an instance of the app structure
+	app := NewApp()
 
-	const chunkSize = 4 * 1024
+	// Create application with options
+	err := wails.Run(&options.App{
+		Title:  "ayo",
+		Width:  1024,
+		Height: 768,
+		AssetServer: &assetserver.Options{
+			Assets: assets,
+		},
+		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+		OnStartup:        app.startup,
+		Bind: []interface{}{
+			app,
+		},
+	})
 
-	buffer := make([]byte, chunkSize)
-	var index int64 = 0
-
-	for {
-		n, err := file.Read(buffer)
-
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			panic(fmt.Sprintf("Failed to read file: %v", err))
-		}
-
-		chunk := buffer[:n]
-
-		fmt.Println("Chunk: ", len(chunk))
-		WriteChunk(index, chunk)
-		index++
+	if err != nil {
+		println("Error:", err.Error())
 	}
-
-	ReconstructFromChunks()
 }
